9fd53620c996bc4dce4815ace983112d4d2657c6
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2020-11-09 17:52:19 +0000

    Make URL end with exactly one slash

---
+++ SCEnterpriseStoryTags.Tests/Converters/SiteUrlValidationConverterTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SCEnterpriseStoryTags.Converters;
+
+namespace SCEnterpriseStoryTags.Tests.Converters
+{
+    [TestFixture]
+    public class SiteUrlValidationConverterTests
+    {
+        [TestCase("http://url")]
+        [TestCase("http://url/")]
+        [TestCase("http://url///")]
+        public void UrlEndsWithExactlyOneSlash(string url)
+        {
+            // Arrange
+
+            var converter = new SiteUrlValidationConverter();
+
+            // Act
+
+            var result = (string) converter.ConvertBack(url, null, null, null);
+
+            // Assert
+
+            Assert.AreEqual("http://url/", result);
+        }
+    }
+}
---
+++ SCEnterpriseStoryTags/Converters/SiteUrlValidationConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SCEnterpriseStoryTags.Converters
+{
+    public class SiteUrlValidationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string url)
+            {
+                var trimmed = url.TrimEnd('/');
+                return trimmed + "/";
+            }
+
+            return value;
+        }
+    }
+}
